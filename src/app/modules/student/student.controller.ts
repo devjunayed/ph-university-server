cf6b8a3b10e55efb902c00d6e93f327403ae0899
Md Junayed <devjunayed@gmail.com>
2024-07-16 12:13:44 +0600

    catchAsync utils funciton createdj

--- src/app/modules/student/student.controller.ts
+++ src/app/modules/student/student.controller.ts
@@ -1,53 +1,40 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import { NextFunction, Request, Response } from 'express'
+import { Request, Response } from 'express'
 
 import { StudentServices } from './student.service'
+import catchAsync from '../../utils/catchAsync'
 
-const getSingleStudent = async (req: Request, res: Response, next: NextFunction) => {
-    try {
-        const { studentId } = req.params
-        const result = await StudentServices.getSingleStudentFromDB(studentId)
+const getSingleStudent = catchAsync(async (req: Request, res: Response) => {
+    const { studentId } = req.params
+    const result = await StudentServices.getSingleStudentFromDB(studentId)
 
-        res.status(200).json({
-            success: true,
-            message: 'Student is data retrieved successfully',
-            data: result,
-        })
-    } catch (err: any) {
-      next(err);
-       
-    }
-}
+    res.status(200).json({
+        success: true,
+        message: 'Student is data retrieved successfully',
+        data: result,
+    })
+})
 
-const getAllStudents = async (req: Request, res: Response, next: NextFunction) => {
-    try {
-        const result = await StudentServices.getAllStudentsFromDB()
+const getAllStudents = catchAsync(async (req: Request, res: Response) => {
+    const result = await StudentServices.getAllStudentsFromDB()
 
-        res.status(200).json({
-            success: true,
-            message: 'Students retrived successfully',
-            data: result,
-        })
-    } catch (err) {
-      next(err);
-    }
-}
+    res.status(200).json({
+        success: true,
+        message: 'Students retrived successfully',
+        data: result,
+    })
+})
 
-const deleteStudent = async (req: Request, res: Response, next: NextFunction) => {
-    try {
-        const { studentId } = req.params
-        const result = await StudentServices.deleteStudentFromDB(studentId)
+const deleteStudent = catchAsync(async (req: Request, res: Response) => {
+    const { studentId } = req.params
+    const result = await StudentServices.deleteStudentFromDB(studentId)
 
-        res.status(200).json({
-            success: true,
-            message: 'Student is deleted successfully',
-            data: result,
-        })
-    } catch (err) {
-        next(err);
-
-    }
-}
+    res.status(200).json({
+        success: true,
+        message: 'Student is deleted successfully',
+        data: result,
+    })
+})
 
 export const StudentControllers = {
     getAllStudents,
--- src/app/modules/user/user.controller.ts
+++ src/app/modules/user/user.controller.ts
@@ -1,31 +1,22 @@
-import { NextFunction, Request, Response } from 'express'
 import { UserService } from './user.service'
 import { sendResponse } from '../../utils/sendResponse'
 import httpStatus from 'http-status'
+import catchAsync from '../../utils/catchAsync'
 
-const createStudent = async (
-    req: Request,
-    res: Response,
-    next: NextFunction
-) => {
-    try {
-        const { password, student: studentData } = req.body
 
-        const result = await UserService.createStudentIntoDB(
-            password,
-            studentData
-        )
 
-        sendResponse(res, {
-            statusCode: httpStatus.OK,
-            success: true,
-            message: "Student created successfully",
-            data: result
-        })
-    } catch (err) {
-        next(err);
-    }
-}
+const createStudent = catchAsync(async (req, res) => {
+    const { password, student: studentData } = req.body
+
+    const result = await UserService.createStudentIntoDB(password, studentData)
+
+    sendResponse(res, {
+        statusCode: httpStatus.OK,
+        success: true,
+        message: 'Student created successfully',
+        data: result,
+    })
+})
 
 export const UserController = {
     createStudent,
