175317b08d507fde2870d788b247acdb190d4e8e
Md Junayed <devjunayed@gmail.com>
2024-08-18 19:16:52 +0600

    deleting user using transaction and rollback is done

--- src/app/modules/student/student.service.ts
+++ src/app/modules/student/student.service.ts
@@ -1,4 +1,8 @@
+import mongoose from 'mongoose'
 import { Student } from './student.model'
+import httpStatus from 'http-status'
+import AppError from '../../errors/AppError'
+import User from '../user/user.model'
 
 const getAllStudentsFromDB = async () => {
     const result = await Student.find()
@@ -26,8 +30,39 @@ const getSingleStudentFromDB = async (id: string) => {
 }
 
 const deleteStudentFromDB = async (id: string) => {
-    const result = await Student.updateOne({ id }, { isDeleted: true })
-    return result
+    const session = await mongoose.startSession()
+    try {
+        const deletedStudent = await Student.findOneAndUpdate([
+            { id },
+            { isDeleted: true },
+            { new: true, session },
+        ])
+
+        if (!deletedStudent) {
+            throw new AppError(
+                httpStatus.BAD_REQUEST,
+                'Failed to delete student'
+            )
+        }
+
+        const deletedUser = await User.findOneAndUpdate(
+            { id },
+            { isDeleted: true },
+            { new: true, session }
+        )
+
+        if (!deletedUser) {
+            throw new AppError(httpStatus.BAD_REQUEST, 'Failed to delete user')
+        }
+
+        await session.commitTransaction()
+        await session.endSession()
+
+        return deletedStudent
+    } catch (error) {
+        await session.abortTransaction()
+        await session.endSession()
+    }
 }
 
 export const StudentServices = {
--- src/app/modules/user/user.validation.ts
+++ src/app/modules/user/user.validation.ts
@@ -14,3 +14,4 @@ const userValidationSchema = z.object({
 export const UserValidationSchema = {
     userValidationSchema,
 }
+
