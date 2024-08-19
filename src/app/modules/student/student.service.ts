c8302bc782aa03b4e44a2867ae24e49768f1ea77
Md Junayed <devjunayed@gmail.com>
2024-08-19 21:54:13 +0600

    module 13 is finished

--- src/app/modules/student/student.service.ts
+++ src/app/modules/student/student.service.ts
@@ -18,7 +18,7 @@ const getAllStudentsFromDB = async () => {
 
 const getSingleStudentFromDB = async (id: string) => {
     // const result = await Student.aggregate([{ $match: { id } }])
-    const result = await Student.findById(id)
+    const result = await Student.findOne({id})
         .populate('admissionSemester')
         .populate({
             path: 'academicDepartment',
