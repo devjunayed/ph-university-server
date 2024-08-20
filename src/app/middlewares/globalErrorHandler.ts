59eb5e01f1ec3f5a07f78051ceed2d842c8d01f6
Md Junayed <devjunayed@gmail.com>
2024-08-20 21:38:27 +0600

    video 1, 2 done of module 14

--- src/app/middlewares/globalErrorHandler.ts
+++ src/app/middlewares/globalErrorHandler.ts
@@ -1,23 +1,55 @@
-/* eslint-disable no-unused-vars */
 /* eslint-disable @typescript-eslint/no-unused-vars */
-/* eslint-disable @typescript-eslint/no-explicit-any */
-import { NextFunction, Request, Response } from 'express'
+/* eslint-disable no-unused-vars */
+import { ErrorRequestHandler, NextFunction, Request, Response } from 'express'
+import { ZodError } from 'zod'
 
-const globalErrorHandler = (
-    err: any,
-    req: Request,
-    res: Response,
-    next: NextFunction
-) => {
-    const statusCode = err.statusCode || 500
-    const message = err.message || 'Something went wrong'
+const globalErrorHandler: ErrorRequestHandler = (err, req, res, next) => {
+    // setting default values
+    let statusCode = err.statusCode || 500
+    let message = err.message || 'Something went wrong'
+
+    type TErrorSource = {
+        path: string | number
+        message: string
+    }
+
+    const errorSources: TErrorSource[] = [
+        {
+            path: '',
+            message: 'Something went wrong',
+        },
+    ]
+
+
+    const handleZodError = (err: ZodError) => {
+        const statusCode = 400;
+
+        return {
+            
+        }
+    }
+
+    if (err instanceof ZodError) {
+        statusCode = 400
+        message = 'zod error'
+    }
 
     return res.status(statusCode).json({
         success: false,
         message,
-        error: err,
+        errorSources,
     })
 }
 
+// patern
+/**
+ * success
+ * message
+ * errorSources : [
+ *     path: '',
+ *     message: ''
+ * ],
+ * stack
+ */
 
-export default globalErrorHandler;
+export default globalErrorHandler
