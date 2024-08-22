49ff502a155e7f0f1ec021c001372761b2085679
Md Junayed <devjunayed@gmail.com>
2024-08-22 17:05:43 +0600

    error and apperror handled and uncaughtexception and unhandledRejection done

--- src/app/middlewares/globalErrorHandler.ts
+++ src/app/middlewares/globalErrorHandler.ts
@@ -7,6 +7,7 @@ import config from '../config'
 import handleZodError from '../errors/handleZodError'
 import handleValidationError from '../errors/handleValidationError'
 import handleDuplicateError from '../errors/handleDuplicateError'
+import AppError from '../errors/AppError'
 
 const globalErrorHandler: ErrorRequestHandler = (err, req, res, next) => {
     // setting default values
@@ -51,6 +52,27 @@ const globalErrorHandler: ErrorRequestHandler = (err, req, res, next) => {
         statusCode = simplifiedError.statusCode
         message = simplifiedError.message
         errorSources = simplifiedError.errorSources
+    }
+    // app error
+    else if(err instanceof AppError){
+        statusCode = err.statusCode
+        message = err.message
+        errorSources = [
+            {
+                path: '',
+                message: err.message
+            }
+        ]
+    }
+    // error
+    else if(err instanceof Error){
+        message = err.message
+        errorSources = [
+            {
+                path: '',
+                message: err.message
+            }
+        ]
     }
 
     // returning the error
--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,31 @@
-import mongoose from "mongoose";
-import app from "./app";
-import config from "./app/config";
+/* eslint-disable no-console */
+import mongoose from 'mongoose'
+import app from './app'
+import config from './app/config'
+import { Server } from 'http'
 
+let server: Server
 
-async function main () {
-   await mongoose.connect(config.db_url as string);
-   app.listen(config.port, () => {
-    console.log(`server is running at port ${config.port}`);
-   })
+async function main() {
+    await mongoose.connect(config.db_url as string)
+    server = app.listen(config.port, () => {
+        console.log(`server is running at port ${config.port}`)
+    })
 }
-main();
+main()
+
+process.on('unhandledRejection', () => {
+    console.log('unhandledRejection is detected, shutting down ...')
+    if (server) {
+        server.close(() => {
+            process.exit(1)
+        })
+    }
+    process.exit(1)
+})
+
+process.on('uncaughtException', () => {
+    console.log('uncaughtException is detected, shutting down ...')
+
+    process.exit(1)
+})
